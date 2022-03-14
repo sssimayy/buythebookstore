406fa465718d5836eb847cb01212e358d27523cb
Simay <simay95@hotmail.com>
2022-03-14 19:11:04 +0300

    Daos have been created

---
+++ src/main/java/com/buythebookstore/dataAccess/UserDao.java
@@ -0,0 +1,7 @@
+package com.buythebookstore.dataAccess;
+
+import com.buythebookstore.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDao extends JpaRepository<User,Integer> {
+}
