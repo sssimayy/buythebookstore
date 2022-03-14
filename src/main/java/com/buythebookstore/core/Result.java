bd025bfd92dd376e252d0473434d8cff6884ccb0
Simay <simay95@hotmail.com>
2022-03-14 19:12:57 +0300

    Result classes have been created

---
+++ src/main/java/com/buythebookstore/core/Result.java
@@ -0,0 +1,20 @@
+package com.buythebookstore.core;
+
+
+import lombok.Getter;
+
+@Getter
+public class Result {
+
+    private boolean success;
+    private String message;
+
+    public Result(boolean success) {
+        this.success = success;
+    }
+
+    public Result(boolean success, String message) {
+        this(success);
+        this.message = message;
+    }
+}
