099478488439beaa5725ae6039839995d20cf8d1
Simay <simay95@hotmail.com>
2022-03-15 14:03:01 +0300

    kind class has been added

---
+++ src/main/java/com/buythebookstore/entities/Kind.java
@@ -0,0 +1,26 @@
+package com.buythebookstore.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "kinds")
+public class Kind {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "userName")
+    private String kind;
+
+    @OneToOne
+    private Book book;
+}
