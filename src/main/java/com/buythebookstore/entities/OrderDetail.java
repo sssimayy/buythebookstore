39e250a2e31401e692e021796a81f55acde33b62
Simay <simay95@hotmail.com>
2022-03-15 14:02:44 +0300

    order classes have been changed

---
+++ src/main/java/com/buythebookstore/entities/OrderDetail.java
@@ -0,0 +1,26 @@
+package com.buythebookstore.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToOne
+    private Book book;
+
+    @ManyToOne
+    private Order order;
+
+    private int number;
+}
