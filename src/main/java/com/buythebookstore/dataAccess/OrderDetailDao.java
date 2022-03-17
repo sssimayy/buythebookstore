0c6caec566134a50556a51f1f7feaca27a8d889d
Simay <simay95@hotmail.com>
2022-03-17 20:26:20 +0300

    Query is added

--- src/main/java/com/buythebookstore/dataAccess/OrderDetailDao.java
+++ src/main/java/com/buythebookstore/dataAccess/OrderDetailDao.java
@@ -2,7 +2,10 @@ package com.buythebookstore.dataAccess;
 
 import com.buythebookstore.entities.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface OrderDetailDao extends JpaRepository<OrderDetail, Integer> {
 
+    @Query("Select sum(o.number) From OrderDetail o  where  o.book.id=:bookId")
+    Integer getTotalSellNumberByBookId(int bookId);
 }
