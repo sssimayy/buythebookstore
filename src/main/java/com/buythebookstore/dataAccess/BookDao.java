2aecff336b94ec3ed7514ec2a3a946421450c1b6
Simay <simay95@hotmail.com>
2022-03-17 20:22:48 +0300

    Query is added

--- src/main/java/com/buythebookstore/dataAccess/BookDao.java
+++ src/main/java/com/buythebookstore/dataAccess/BookDao.java
@@ -1,7 +1,7 @@
 package com.buythebookstore.dataAccess;
 
 import com.buythebookstore.entities.Book;
-import com.buythebookstore.entities.dtos.BookDto;
+import com.buythebookstore.entities.dtos.BookRecommendationDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,7 +11,7 @@ public interface BookDao extends JpaRepository<Book, Integer> {
 
     Book getById(int id);
 
-    @Query("Select new  com.buythebookstore.entities.dtos.BookDto(b.id,b.bookName,b.bookNumber,b.bookPrice) From Book b Inner Join b.orderDetail o " +
-            "where  b.kind=:kind order by o.number desc")
-    List<BookDto> findAllByKindOrderByNumberDesc(String kind);
+    @Query(value = "Select new com.buythebookstore.entities.dtos.BookRecommendationDto(b.id, b.bookName, b.bookNumber, b.bookPrice) From Book b Inner Join b.kind k " +
+            "where  k.kind=:kind")
+    List<BookRecommendationDto> findAllByKind(String kind);
 }
